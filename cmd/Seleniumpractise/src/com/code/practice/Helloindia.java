4d841e5a8927fd569e0e3ce7eeab4777f0daaa22
preethikishor <preethidj@gmail.com>
2017-07-29 20:44:48 +0530

    !!! added

--- cmd/Seleniumpractise/src/com/code/practice/Helloindia.java
+++ cmd/Seleniumpractise/src/com/code/practice/Helloindia.java
@@ -3,7 +3,7 @@ package com.code.practice;
 public class Helloindia {
 
 	public static void main(String[] args) {
-	    System.out.println("India sucess+++");
+	    System.out.println("India sucess!!!");
 
 	}
 
